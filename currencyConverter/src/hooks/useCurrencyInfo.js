2af442fb076f30dfb16424fd57fb75303bcf4e29
Pushpendra singh <pankaj182007@gmail.com>
2024-08-03 16:13:16 +0530

    currency converter completed

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+
+    const [data,seData]=useState({})
+
+   let url=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`   
+    useEffect(()=>{
+        fetch(url)
+        .then((res)=>res.json())
+        .then((res)=>seData(res[currency]))
+        console.log(data)
+    },[currency])
+    return data
+}
+export default useCurrencyInfo;
